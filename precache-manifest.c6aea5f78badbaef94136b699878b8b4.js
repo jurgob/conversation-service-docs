1583b80cbe179a519f0365eb9da8062e1e640b40
Jurgo Boemo <jurgo.boemo@gmail.com>
2019-08-16 16:09:59 +0100

    Updates

---
+++ precache-manifest.c6aea5f78badbaef94136b699878b8b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0c06d952024d41bfb35f6f0532b82f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce2ad680288ead0f1672",
+    "url": "/static/css/2.5a3c8dd4.chunk.css"
+  },
+  {
+    "revision": "0390392d09261b09ac45",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "ce2ad680288ead0f1672",
+    "url": "/static/js/2.61e9d681.chunk.js"
+  },
+  {
+    "revision": "0390392d09261b09ac45",
+    "url": "/static/js/main.3ee0e924.chunk.js"
+  },
+  {
+    "revision": "733ab1f5ecdbe62fff57",
+    "url": "/static/js/runtime~main.59d17bf9.js"
+  }
+]);
