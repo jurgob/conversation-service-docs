3b6f5956f510dc818ba10ac5ce3422dbee0c6df8
Jurgo Boemo <jurgo.boemo@gmail.com>
2020-01-29 15:24:30 +0000

    enrich user_list reponse object

--- src/merge.js
+++ src/merge.js
@@ -1,15 +1,18 @@
 const fs = require('fs');
 
-const spec1_content = fs.readFileSync('./openapi_specs/specs.openapi_v1.json')
+const spec1_content = fs.readFileSync('./src/openapi_specs/specs.openapi_v1.json')
 const spec1 = JSON.parse(spec1_content)
 
-const spec2_content = fs.readFileSync('./openapi_specs/specs.openapi_v2.json')
+const spec2_content = fs.readFileSync('./src/openapi_specs/specs.openapi_v2.json')
 const spec2 = JSON.parse(spec2_content)
 
 const spec_result = JSON.parse(JSON.stringify(spec1))
 
 //merge
+if (!spec_result.servers[0]) spec_result.servers[0] = {};
+
 spec_result.servers[0].url = "https://api.nexmo.com"
+
 Object.entries(spec_result.paths)
 	.filter(([path, path_verbs]) => !path.includes('/applications'))
 	.forEach(([path, path_verbs]) => {
@@ -52,4 +55,4 @@ Object.entries(spec_result.paths)
 
 
 // console.log(JSON.stringify(spec_result, ' ', ' '))
-fs.writeFileSync('specs.openapi_all.json', JSON.stringify(spec_result, ' ', ' '));
+fs.writeFileSync('./src/openapi_specs/specs.openapi_all.json', JSON.stringify(spec_result, ' ', ' '));
